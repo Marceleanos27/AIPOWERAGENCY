df8fbcbdfcc82663163d48194712687b12b82c89
Marceleanos27 <lehocky.business@gmail.com>
2025-08-22 23:18:58 +0200

    Update chatbot-widget.js

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -3,7 +3,7 @@
   window.__marcelChatbotLoaded = true;
 
   const iframe = document.createElement("iframe");
-  iframe.src = "https://grandenassistantai-sigma.vercel.app/"; // ⬅️ Change to your real URL (e.g. Vercel)
+  iframe.src = "https://aipoweragency.vercel.app/chatbot-widget.js"; // ⬅️ Change to your real URL (e.g. Vercel)
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
@@ -15,6 +15,7 @@
   iframe.style.boxShadow = "0 10px 25px rgba(0,0,0,0.0)";
   document.body.appendChild(iframe);
 })();
+
 
 
 
